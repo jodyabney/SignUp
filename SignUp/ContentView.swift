8b071881ae6398502ccc816f0c27675cc7e1eb46
Jody Abney <jodyabney@gmail.com>
2020-07-24 18:10:53 -0400

    Initial Commit

---
+++ SignUp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SignUp
+//
+//  Created by Jody Abney on 7/24/20.
+//  Copyright © 2020 AbneyAnalytics. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
